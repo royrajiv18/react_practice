fc3dfc63f492fdbbca5898790e71b9d6c522ad85
Rajiv Roy <rajivpune18@gmail.com>
2026-01-28 21:50:38 +0530

    Implement props drilling and context API examples
    Added examples of props drilling and context API usage in React components.

--- practice.js
+++ practice.js
@@ -1,1 +1,79 @@
+// practice
+//props drilling examples
+//  in App.jsx
 
+const App = ()=> {
+	const userName = 'Rajiv';
+	return (
+	<div>
+		<Parent name = {userName}/>
+	</div>
+)
+}
+export default App;
+
+// in Parent.jsx
+
+const Parent = ({name})=>{
+	return (
+	<div>
+		<Child name={name}/>
+	</div>
+)
+}
+export default Child;
+
+// in Child.jsx
+
+const Child = ({name})=> {
+	return (
+	<div>
+		<h2>Hello, {name}!</h2>
+	</div>
+)
+}
+export default Child;
+
+// context api
+// create context, provide context, consume context
+
+// create context
+
+// in userContext.jsx
+
+export const UserContext = createContext();
+
+// provider context
+
+// in App.s
+
+const App = ()=> {
+	const userName = 'Rajiv';
+	return (
+	<UserContext.Provider value={userName}>
+		<Parent />
+	</UserContext.Provider>
+)
+}
+export default App
+// Parent.js
+
+const Parent = ()=> {
+return (
+	<Child />
+)
+}
+export default Parent
+
+// Child.js // consume context
+
+import {useContext} from 'react';
+import {UserContext} from './UserContext'
+
+const Child = ()=> {
+	const name = useContext(UserContext);
+	return (
+	<h2>Hello, {name}!</h2>
+)
+}
+export default Child
